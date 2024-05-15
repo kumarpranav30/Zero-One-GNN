ad6813460b848299aa8527b732db0d5ae34f2b96
Pranav Kumar <pranavkumar.del@gmail.com>
2024-05-15 17:23:21 +0530

    Updated simulate.py

--- Code/simulate.py
+++ Code/simulate.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 from torch_geometric.utils.convert import from_networkx
 import random
 
-emb_size = 8
+node_emb_size = 128
 
 class GCN(torch.nn.Module):
     def __init__(self):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(emb_size, 128) 
+        self.conv1 = GCNConv(node_emb_size, 128) 
         self.conv2 = GCNConv(128, 32)  # Output a single feature per node
         self.fc1 = torch.nn.Linear(32, 16)  # First MLP layer
         self.fc2 = torch.nn.Linear(16, 1)
@@ -23,26 +23,26 @@ class GCN(torch.nn.Module):
         x = F.relu(self.conv1(x, edge_index))
         x = F.relu(self.conv2(x, edge_index))
         x = global_mean_pool(x, batch)  # Mean pooling
-        x = F.relu(self.fc1(x))  # First MLP layer with ReLU
+        x = torch.tanh(self.fc1(x))  # First MLP layer with tanh
         x = self.fc2(x)  # Second MLP layer
         return torch.sigmoid(x)  # Sigmoid activation at the output
 
     def initialize_weights(self):
-        # Initialize weights for each GCNConv layer
+        # Initialize weights for each Convolutional layer
         for m in self.modules():
             if isinstance(m, GCNConv):
-                torch.nn.init.uniform_(m.lin.weight, -1, 1)  # Correct attribute for weight
+                torch.nn.init.uniform_(m.lin.weight, -1, 1)  # Set random weights and biases
                 if m.lin.bias is not None:
                     torch.nn.init.uniform_(m.lin.bias, -1, 1)
 
 def generate_graph(n, p=0.5):
     G = nx.erdos_renyi_graph(n, p)
     for _, node_data in G.nodes(data=True):
-        node_data['x'] = torch.tensor([random.uniform(0,1) for x in range(emb_size)])  # Set random node-embeddings
+        node_data['x'] = torch.tensor([random.uniform(0,1) for x in range(node_emb_size)])  # Set random node-embeddings
     data = from_networkx(G)
     return data
 
-iterations = 20 
+iterations = 10 # Number of random GNNs to test the law on
 plt.figure(figsize=(10, 6))
 
 for num_itr in range(iterations):
@@ -51,8 +51,8 @@ for num_itr in range(iterations):
     model = GCN().to(device)
     model.eval()  # Set the model to evaluation mode
     outputs = []
-    ns = range(1, 62, 2)
-    samples = 15
+    ns = list(range(1, 30, 1)) + list(range(30, 150, 15))
+    samples = 32
     for n in ns:
         print(f'Nodes = {n}')
         cnt = 0
